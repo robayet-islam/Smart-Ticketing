5b93f237802b0af97b29ffb4659f1e6cc1e1cd91
robaed-islam <robaed.islam.071@gmail.com>
2024-02-18 22:36:07 +0600

    Some funtional done!

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-function buyTicket () {
-    const bannerBuyTicket = document.getElementById('banner-buy-ticket')
-    
-    bannerBuyTicket.addEventListener ("click", function(){
-        // const phParibahan = getElementById ('PH-paribahan');
-        const removeBannerByTicket = getElementById ('bannder');
-        const removeBanner = removeBannerByTicket.classList.add("hidden");;
-        // const addPhParibahan = phParibahan;
+let selectedSeat = 0;
+const seats = document.querySelectorAll('.seat');
+for(const seat of seats){
+    seat.addEventListener("click", function(){
+        selectedSeat++;
+        const countSeat = document.getElementById("count-seat");
+        countSeat.innerText = selectedSeat;
+        const totalSeat = parseInt(document.getElementById("total-seat").innerText);
+        const totalAbilable = totalSeat-1;
+        document.getElementById("total-seat").innerText = totalAbilable;
+        seat.classList.add("bg-green-500");
+        const seatInfo = document.getElementById("seat-info");
+        
+
     })
 }
 
