3a63f70ff48b10d292f5d3897599c4db087d792e
robaed-islam <robaed.islam.071@gmail.com>
2024-02-19 09:46:12 +0600

    responsive added

--- script/script.js
+++ script/script.js
@@ -11,6 +11,31 @@ for(const seat of seats){
         seat.classList.add("bg-green-500");
         const seatInfo = document.getElementById("seat-info");
         
+        const div = document.createElement("div");
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+        p1.innerText = seat.innerText;
+        p2.innerText = " Economoy";
+        p3.innerText = 550;
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        seatInfo.appendChild(div);
+        // ////////////////////////////////////////////////////////////////////////
+        let totalPrice = document.getElementById('total-price').innerText;
+        const singleSeatPrice = 550;
+        totalPrice = singleSeatPrice * selectedSeat;
+        document.getElementById('total-price').innerText = totalPrice;
+
+       
+
+       
+        
+
+        
+
+        
 
     })
 }
